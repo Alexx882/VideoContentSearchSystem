5f6d98bd7eac0cf6d9a5bc190801b613ec1f30a9
Alexander Lercher <allercher@edu.aau.at>
2020-07-14 00:01:26 +0200

    Running shot detection for all provided videos

--- main.cpp
+++ main.cpp
@@ -4,5 +4,21 @@
 
 int main(int argc, char** argv)
 {
-	return mainSvmClassifier(argc, argv);
+	const char* videos[10][3] = {
+		{ "", "videos\\v3c1\\00492.mp4", "keyframes\\v3c1\\00492" },
+		{ "", "videos\\v3c1\\01729.mp4", "keyframes\\v3c1\\01729" },
+		{ "", "videos\\v3c1\\01736.mp4", "keyframes\\v3c1\\01736" },
+		{ "", "videos\\v3c1\\03847.mp4", "keyframes\\v3c1\\03847" },
+		{ "", "videos\\v3c1\\05814.mp4", "keyframes\\v3c1\\05814" },
+		{ "", "videos\\v3c1\\05979.mp4", "keyframes\\v3c1\\05979" },
+		{ "", "videos\\v3c1\\06385.mp4", "keyframes\\v3c1\\06385" },
+		{ "", "videos\\v3c1\\07031.mp4", "keyframes\\v3c1\\07031" },
+		{ "", "videos\\v3c1\\07037.mp4", "keyframes\\v3c1\\07037" },
+		{ "", "videos\\v3c1\\07187.mp4", "keyframes\\v3c1\\07187" }
+	};
+
+	for (int i = 0; i < 10; ++i)
+		mainShotDetection(3, videos[i]);
+
+	return 0;	
 }
--- main1.cpp
+++ main1.cpp
@@ -12,17 +12,19 @@ using namespace std;
 namespace fs = std::filesystem;
 using namespace cv;
 
+const char* FOLDER_NAME = "keyframes";
+
 void storeKeyFrameForVideoShot(VideoCapture cap, int shotNr, int frameNr)
 {	
 	Mat frame;
 	cap.set(CAP_PROP_POS_FRAMES, frameNr);
 	cap.read(frame);
 	
-	string path = format("keyframes\\shot%d_frame%d.png", shotNr, frameNr);
+	string path = format("%s\\shot%d_frame%d.png", FOLDER_NAME, shotNr, frameNr);
 	cv::imwrite(path, frame);
 }
 
-int mainShotDetection(int argc, char** argv)
+int mainShotDetection(int argc, const char** argv)
 {
 	if (argc < 2)
 	{
@@ -30,6 +32,9 @@ int mainShotDetection(int argc, char** argv)
 		exit(0);
 	}
 
+	if (argc == 3)
+		FOLDER_NAME = argv[2];
+	
 	VideoCapture cap = VideoCapture(argv[1]);
 	FrameComparer comparer = FrameComparer(64);
 	VideoShotDescriptor shotDescr = VideoShotDescriptor();
@@ -50,8 +55,8 @@ int mainShotDetection(int argc, char** argv)
 		if (count > 1 && comparer.isNextTwinComparisonExceeded(prevHist, curHist))
 			shotDescr.addBoundary(count);
 
-		cv::imshow("Video", frame);
-		waitKey(waitTime);
+		//cv::imshow("Video", frame);
+		//waitKey(waitTime);
 
 		++count;
 	}
@@ -59,8 +64,8 @@ int mainShotDetection(int argc, char** argv)
 
 	shotDescr.printShots();
 
-	fs::remove_all("keyframes");
-	fs::create_directory("keyframes"); // use mkdir(folderName) for linux
+	fs::remove_all(FOLDER_NAME);
+	fs::create_directories(FOLDER_NAME); // use mkdir(folderName) for linux
 	vector<int> keyframes = shotDescr.getKeyFrameNumbers();
 	for (int i = 0; i < keyframes.size(); ++i)
 		storeKeyFrameForVideoShot(cap, i, keyframes[i]);
