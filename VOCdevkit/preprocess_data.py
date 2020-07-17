707e47d6188ba0f15bdbfa798bfb5df31c202d48
Alexander Lercher <allercher@edu.aau.at>
2020-07-17 19:53:35 +0200

    Training with 10 concepts from VOC2012
    Preparation scripts moves the images to folders 'training' and 'test' for use with ImageDataGenerator.

--- VOCdevkit/preprocess_data.py
+++ VOCdevkit/preprocess_data.py
@@ -16,20 +16,38 @@ def copy_images_to_labeled_folder():
     This script preprocesses the data from VOCdevkit (http://host.robots.ox.ac.uk/pascal/VOC/voc2012/index.html#devkit).
     It moves images from the same class into the corresponding folder for processing with ImageDataGenerator.flow_from_directory.
     '''
+    data = {}
+    print("Loading images")
     for path, _, files in os.walk(path_normjoin(BASE_PATH, 'JPEGImages')):
         for image_file in files:
 
             # load first label from annotation xml
             xml_doc = ET.parse(path_normjoin(path_normjoin(BASE_PATH, 'Annotations'), image_file.replace('.jpg', '.xml')))
             label = xml_doc.findall('object')[0].findall('name')[0].text
-            print(label)
 
-            # move file to new folder based on label
-            target_folder = path_normjoin(PREPARED_IMAGES_PATH, label)
-            if not os.path.exists(target_folder):
-                os.makedirs(target_folder)
+            if label not in data:
+                data[label] = []
+            data[label].append(image_file)
 
-            shutil.copy(path_normjoin(path, image_file), path_normjoin(target_folder, image_file))
+    cnt = 0
+    for label in data.keys():
+        print(f"Copying images for {label} (concept {cnt+1} of {len(data.keys())})")
+        cnt += 1
+
+        # move files to new folder based on label
+        training_folder = path_normjoin(path_normjoin(PREPARED_IMAGES_PATH, 'training'), label)
+        test_folder = path_normjoin(path_normjoin(PREPARED_IMAGES_PATH, 'test'), label)
+        if not os.path.exists(training_folder):
+            os.makedirs(training_folder)
+            os.makedirs(test_folder)
+
+        num_train_data = int(len(data[label])*.7)
+
+        for image_file in data[label][:num_train_data]:
+            shutil.copy(path_normjoin(path, image_file), path_normjoin(training_folder, image_file))
+            
+        for image_file in data[label][num_train_data:]:
+            shutil.copy(path_normjoin(path, image_file), path_normjoin(test_folder, image_file))
 
 
 def import_concepts_in_db():
@@ -44,7 +62,7 @@ def import_concepts_in_db():
 
     # load all concepts based on folders for ImageDataGenerator.flow_from_directory, which labels the same way
     concepts = []
-    for path, dirs, files in os.walk(PREPARED_IMAGES_PATH):
+    for path, dirs, files in os.walk(path_normjoin(PREPARED_IMAGES_PATH, 'training')):
         if dirs is None or len(dirs) == 0:
             continue
         concepts.extend(dirs)
@@ -63,4 +81,5 @@ def import_concepts_in_db():
 
 
 if __name__ == "__main__":
+    # copy_images_to_labeled_folder()
     import_concepts_in_db()
